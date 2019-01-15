3d797538774ba01845bac5efd7dc2c35b9514b12
Mohd Umar Alam <umaralam48@gmail.com>
2019-01-15 20:29:47 +0530

    Updates

---
+++ precache-manifest.16edd1aabe8a18797d03d0e0632c01f6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b7de66cb1325631b32ee",
+    "url": "/listtracker/static/js/runtime~main.b7de66cb.js"
+  },
+  {
+    "revision": "f5abf1282374d1e0f4d1",
+    "url": "/listtracker/static/js/main.f5abf128.chunk.js"
+  },
+  {
+    "revision": "32c54c8cc177a22cc984",
+    "url": "/listtracker/static/js/1.32c54c8c.chunk.js"
+  },
+  {
+    "revision": "f5abf1282374d1e0f4d1",
+    "url": "/listtracker/static/css/main.25e8a396.chunk.css"
+  },
+  {
+    "revision": "b2796fad66cda6b24bb2d27517fcab5c",
+    "url": "/listtracker/index.html"
+  }
+];
